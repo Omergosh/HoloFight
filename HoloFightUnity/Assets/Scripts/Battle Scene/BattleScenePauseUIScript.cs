f834233364c925da429bfd40ede93dc1809e6786
Omer Ahmed <omera407@gmail.com>
2022-07-24 13:55:17 -0600

    Added UI packages; pause timer UI vastly improved
    -Now, when you begin holding down the pause button (because you have to hold it down for a certain period of time in order to fully initiate a pause), UI elements on screen appear (so you know you pushed the pause button) and fade/fill transition in to indicate how close you are to fully pausing.
    
    -Added Unity package asset: Procedural UI Image
    -- (currently being used for pause timer)
    -Added Unity package asset: UI Gradient
    -Added Unity package asset: Translucent Image - Fast UI Background Blur
    
    Since these packages are paid assets, their files have all been placed in a .gitignore'd folder that will not be uploaded to the GitHub repository.
    
    If you download this project and need help setting those packages up or resolving issues related to them, let me know!

--- HoloFightUnity/Assets/Scripts/Battle Scene/BattleScenePauseUIScript.cs
+++ HoloFightUnity/Assets/Scripts/Battle Scene/BattleScenePauseUIScript.cs
@@ -3,16 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
+using UnityEngine.UI.ProceduralImage;
 
 public class BattleScenePauseUIScript : MonoBehaviour
 {
     public Image fullscreenOverlay;
     public Color fullscreenOverlayColor;
     public GameObject pauseMenuPanel;
+    public ProceduralImage pauseTimerFillShape;
+
+    public float pauseTimerFillTimeLimit = 0.4f;
+    private float _pauseProgress= 0f;
+    public float PauseProgress // value range: [0f, 1f]
+    {
+        get { return _pauseProgress; }
+        set
+        {
+            if (value > 1f) { _pauseProgress = 1f; }
+            else if (value < 0f) { _pauseProgress = 0f; }
+            else { _pauseProgress = value; }
+
+            UpdatePauseProgressUI(); // Pretty sure this is bad practice so consider removing/reworking this line later
+        }
+    }
+
+    public bool initiatingPause = false;
 
     public void Start()
     {
+        fullscreenOverlay.gameObject.SetActive(false);
+        pauseMenuPanel.gameObject.SetActive(false);
+        pauseTimerFillShape.gameObject.SetActive(false);
+
         fullscreenOverlayColor = fullscreenOverlay.color;
+        PauseProgress = 0f;
+    }
+
+    private void Update()
+    {
+        if (initiatingPause)
+        {
+            PauseProgress += Time.deltaTime / pauseTimerFillTimeLimit;
+        }
+    }
+
+    public void UpdatePauseProgressUI()
+    {
+        pauseTimerFillShape.fillAmount = PauseProgress;
+        fullscreenOverlay.color = new Color(
+                fullscreenOverlayColor.r,
+                fullscreenOverlayColor.g,
+                fullscreenOverlayColor.b,
+                //fullscreenOverlayColor.a * 0.5f
+                fullscreenOverlayColor.a * PauseProgress
+            );
+        Debug.Log(fullscreenOverlayColor.a);
     }
 
     public void UpdateUI(InputAction pauseInputAction, bool currentlyPaused)
@@ -60,27 +105,42 @@ public class BattleScenePauseUIScript : MonoBehaviour
                 fullscreenOverlayColor.r,
                 fullscreenOverlayColor.g,
                 fullscreenOverlayColor.b,
-                fullscreenOverlayColor.a * 0.5f
+                //fullscreenOverlayColor.a * 0.5f
+                fullscreenOverlayColor.a * PauseProgress
             );
         fullscreenOverlay.gameObject.SetActive(true);
+
+        PauseProgress += Time.deltaTime / pauseTimerFillTimeLimit;
+        pauseTimerFillShape.gameObject.SetActive(true);
+
+        initiatingPause = true;
     }
 
     public void AbortPause()
     {
         // Just let go of the pause button without holding it down long enough to fully pause the game.
+        PauseProgress = 0f;
         fullscreenOverlay.gameObject.SetActive(false);
+        pauseTimerFillShape.gameObject.SetActive(false);
+        initiatingPause = false;
     }
 
     public void Pause()
     {
+        PauseProgress = 0f;
         fullscreenOverlay.color = fullscreenOverlayColor;
         fullscreenOverlay.gameObject.SetActive(true);
         pauseMenuPanel.gameObject.SetActive(true);
+        pauseTimerFillShape.gameObject.SetActive(false);
+        initiatingPause = false;
     }
 
     public void Unpause()
     {
+        PauseProgress = 0f;
         fullscreenOverlay.gameObject.SetActive(false);
         pauseMenuPanel.gameObject.SetActive(false);
+        pauseTimerFillShape.gameObject.SetActive(false);
+        initiatingPause = false;
     }
 }
