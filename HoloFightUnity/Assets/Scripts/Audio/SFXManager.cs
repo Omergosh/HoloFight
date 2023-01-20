3d4199a4ac10a418cbd15e434241067bbe8c3ab1
Omer Ahmed <omera407@gmail.com>
2023-01-20 15:37:40 -0700

    Updated animation for Ina's AttackB1 (Tentacle Whipcrack)

--- HoloFightUnity/Assets/Scripts/Audio/SFXManager.cs
+++ HoloFightUnity/Assets/Scripts/Audio/SFXManager.cs
@@ -19,6 +19,27 @@ public class SFXManager : MonoBehaviour
     public AudioClip charSelectUnreadySound;
 
     // Battle SFX sounds
+    public AudioClip hitJuicySound;
+    public AudioClip hitCrowbarSound;
+    public AudioClip hitHeavySound;
+    public AudioClip hitVeryHeavySound;
+    public AudioClip whooshQuickSound;
+    public AudioClip whooshSlowSound;
+    public AudioClip whooshHeavySound;
+
+    // Misc/Unassigned sounds
+    public AudioClip surfaceTapSound; //DM-CGS-44
+    public AudioClip whooshSharpSound; //DM-CGS-46
+    public AudioClip whooshBluntSound; //DM-CGS-47
+    public AudioClip bounceSound; //DM-CGS-43
+    public AudioClip fireCrossbowThwackSound; //DM-CGS-39
+    public AudioClip sparkleSound; //DM-CGS-34
+    public AudioClip popSound; //DM-CGS-32
+    public AudioClip flashSparkleSound; //DM-CGS-33
+    public AudioClip warpWeirdSound; //DM-CGS-29
+    public AudioClip tickTockSound; //DM-CGS-04
+    public AudioClip boomThinSound; //DM-CGS-05
+    public AudioClip boomDeepSound; //DM-CGS-10
 
 
     // Ina SFX + voiceline sounds
@@ -29,6 +50,7 @@ public class SFXManager : MonoBehaviour
     {
         switch (soundTag)
         {
+            // Menu UI sounds
             case "menuMove":
                 audioSource.PlayOneShot(menuMoveSound);
                 break;
@@ -53,8 +75,30 @@ public class SFXManager : MonoBehaviour
             case "charSelectUnready":
                 audioSource.PlayOneShot(charSelectUnreadySound);
                 break;
-            
-            
+
+            // Battle SFX sounds
+            case "hitJuicy":
+                audioSource.PlayOneShot(hitJuicySound);
+                break;
+            case "hitCrowbar":
+                audioSource.PlayOneShot(hitCrowbarSound);
+                break;
+            case "hitHeavy":
+                audioSource.PlayOneShot(hitHeavySound);
+                break;
+            case "hitVeryHeavy":
+                audioSource.PlayOneShot(hitVeryHeavySound);
+                break;
+            case "whooshQuick":
+                audioSource.PlayOneShot(whooshQuickSound);
+                break;
+            case "whooshSlow":
+                audioSource.PlayOneShot(whooshSlowSound);
+                break;
+            case "whooshHeavy":
+                audioSource.PlayOneShot(whooshHeavySound);
+                break;
+
             
             default:
                 Debug.Log("Invalid sound effect attempted to be played.");
